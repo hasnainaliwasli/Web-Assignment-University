fa802dfa44698d6ffd5d06351574f9be33ca8dca
hasnainaliwasli <hasnain62712@gmail.com>
2024-04-06 13:39:05 +0500

    6th Project

---
+++ Javascript Projects/6.QR Code Generator(Api=goqr.me )/script.js
@@ -0,0 +1,36 @@
+document.getElementById('btn').onclick = () => {
+    let value = document.getElementById('input').value;
+    let link = 'https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=' + encodeURIComponent(value);
+    document.getElementById('img').src = link
+}
+
+
+
+
+// document.getElementById('btn').onclick = () => {
+//     let value = document.getElementById('input').value;
+//     if (!value) {
+//         alert('Input value is empty');
+//         return;
+//     }
+
+//     let link = 'https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=' + encodeURIComponent(value);
+
+//     fetch(link)
+//         .then(response => {
+//             if (!response.ok) {
+//                 throw new Error('Network response was not ok');
+//             }
+//             return response.blob();
+//         })
+//         .then(blob => {
+//             let imgUrl = URL.createObjectURL(blob);
+//             document.getElementById('img').src = imgUrl;
+//         })
+//         .catch(error => {
+//             console.error('Error fetching QR code:', error);
+//         });
+// };
+
+
+
