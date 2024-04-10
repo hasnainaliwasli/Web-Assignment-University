b35160cfe2b3f124cf2aa4aa030f39c3ce28bf0b
hasnainaliwasli <hasnain62712@gmail.com>
2024-04-10 22:14:01 +0500

    project no 8

--- Javascript Projects/8.Password Generator/script.js
+++ Javascript Projects/8.Password Generator/script.js
@@ -1,11 +1,41 @@
 
-
-// Get the range input and output element
 const rangeInput = document.getElementById('rangeInput');
 const outputValue = document.getElementById('output');
+const btn = document.getElementById('button');
 
-// Add event listener to range input
-rangeInput.addEventListener('input', function() {
-    // Update the value of the output element
-    outputValue.textContent = this.value;
+
+
+
+// -------------
+
+function generatePassword(length) {
+    const lowercaseChars = 'abcdefghijklmnopqrstuvwxyz';
+    const uppercaseChars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    const symbols = '!@#$%^&*()-_=+';
+    const numbers = '0123456789';
+
+    let allChars = lowercaseChars + uppercaseChars + symbols + numbers;
+
+    let password = '';
+
+    for (let i = 0; i < length; i++) {
+        const randomIndex = Math.floor(Math.random() * allChars.length);
+        password += allChars[randomIndex];
+    }
+
+    return password;
+}
+
+// Example usage:
+// Length decided by the user
+
+
+btn.addEventListener('click', function () {
+
+    const passwordLength = 12;
+    const newPassword = generatePassword(passwordLength);
+
+    outputValue.innerHTML = newPassword
 });
+
+
