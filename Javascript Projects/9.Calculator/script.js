87e00d0cc9eab9dfe5b2fd9f60516535ea8498ff
hasnainaliwasli <hasnain62712@gmail.com>
2024-04-14 23:40:39 +0500

    9th project Calculator

--- Javascript Projects/9.Calculator/script.js
+++ Javascript Projects/9.Calculator/script.js
@@ -2,35 +2,20 @@ let inputValue = document.getElementById('input');
 let buttons = document.querySelectorAll('button');
 let AllTyped = '';
 
-
 // Convert NodeList to array using Array.from
 Array.from(buttons).forEach(button => {
-
-
     button.addEventListener('click', function (e) {
 
         if (button.value === "=") {
             let final = eval(inputValue.value);
             inputValue.value = final;
             AllTyped = '';
-            console.log(final);
         } else if (button.value !== 'C' && button.value !== '=') {
             AllTyped += button.value;
             inputValue.value = AllTyped;
-            console.log("Value = ", button.value);
-        } else {
-            AllTyped = '';
         }
-
-
+        else {
+            inputValue.value = '';
+        }
     })
-    AllTyped = '';
-
-
 });
-
-
-
-document.getElementById('clear').onclick = () => {
-    inputValue.value = '';
-}
